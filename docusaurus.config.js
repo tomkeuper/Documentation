470c1c002e0726b0a71495897f9fcada9e7d3c09
It'z RJ <86042583+itz-rj-here@users.noreply.github.com>
2024-06-29 12:27:08 +0600

    Update docusaurus.config.js
    The path to "edit this page" on GitHub has been fixed. Since the blog option is not being used, I have added an ignore symbol, so that should be ignored.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,15 +42,15 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/tomkeuper/docs',
-        },
+            'https://github.com/tomkeuper/Documentation/edit/development/',
+/*        },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/tomkeuper/docs',
-        },
+            'https://github.com/tomkeuper/Documentation',
+        },*/
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
